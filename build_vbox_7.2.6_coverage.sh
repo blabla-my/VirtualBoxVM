b6a83b1747bd53fa3eaf07c21fb87e8e0459893d
MingyuanLuo <myluo24@m.fudan.edu.cn>
2026-03-23 16:54:35 +0800

    fix build_vbox_7.2.6_coverage.sh

--- build_vbox_7.2.6_coverage.sh
+++ build_vbox_7.2.6_coverage.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
+wget https://download.virtualbox.org/virtualbox/7.2.6/VirtualBox-7.2.6.tar.bz2 
+tar xvf VirtualBox-7.2.6.tar.bz
 cd VirtualBox-7.2.6
 patch -p1 < ../vbox_with_clang/VirtualBox-7.2.6-clang.patch
 ../vbox_with_clang/build-coverage.sh $PWD
