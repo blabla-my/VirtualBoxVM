bcb8ac7f0532f0c3c8eb7ad19baef95e2164452c
MingyuanLuo <myluo24@m.fudan.edu.cn>
2026-03-23 17:07:00 +0800

    Changes to be committed: 	new file:   dep.sh

---
+++ dep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get install --no-install-recommends acpica-tools chrpath doxygen g++-multilib libasound2-dev libcap-dev \
+        libcurl4-openssl-dev libdevmapper-dev libidl-dev libopus-dev libpam0g-dev \
+        libpng-dev libpulse-dev qt6-base-dev qt6-scxml-dev qt6-tools-dev qt6-l10n-tools libsdl1.2-dev libsdl-ttf2.0-dev \
+        libssl-dev libvpx-dev libxcursor-dev libxinerama-dev libxml2-dev libxml2-utils \
+        libxmu-dev libxrandr-dev linux-source make nasm python3-dev python3-packaging \
+        texlive texlive-fonts-extra texlive-latex-extra unzip xsltproc \
+        default-jdk libstdc++5 libxslt1-dev linux-headers-$(uname -r) makeself \
+        mesa-common-dev subversion yasm zlib1g-dev glslang-tools \
+        libc6-dev-i386 lib32z1 lib32gcc-s1 lib32stdc++6 linux-headers-generic
